ff6e04d621c8282252b27ee477b3c9a910a40478
nooodel <79494124+nooodel@users.noreply.github.com>
2021-05-02 21:48:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,11 @@ function speak() {
     synth.speak(utterThis);
 
     Webcam.attach(camera);
+
+    setTimeout(function() {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 camera = document.getElementById("camera");
@@ -36,3 +41,16 @@ Webcam.set({
     image_format: 'jpeg',
     jpeg_quality: 90
 });
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id ="selfie_image" src="' + data_uri + '"/>';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
